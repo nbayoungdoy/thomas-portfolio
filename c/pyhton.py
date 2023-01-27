9dbdccbc61f3af505ce516668350f36709cdbbbe
thomas <2026veerman.thomas@fcboe.org>
2023-01-27 11:02:33 -0500

    thomas

---
+++ c/pyhton.py
@@ -0,0 +1,18 @@
+print("you spawn in a island do you want to find shelter")
+user = input ("yes/no")
+
+if user == "yes":
+    print("you have shelter, but you are low on food, do you want to go hunting")
+    user = input ("yes/no")
+    if user == "yes":
+        print("you have gotton food it is getting dark do you want to make a fire")
+    elif user == "no":
+        print("while sitting in your shelfter a bear comes and eats you")    
+
+elif user == "no":
+    print("you have no shelter, would you reather go hunting for food")
+    user = input ("yes/no")
+    if user == "yes":
+        print("you have food, it is getting dark do you want to make a fire")
+    elif user == "no":
+        print("you die")
